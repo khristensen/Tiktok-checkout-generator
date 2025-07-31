49095c4fa50f0d37caf8cfd5ee1f01784af7994c
khristensen <khristensen0220@gmail.com>
2025-07-31 01:10:19 -0700

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST", "HEAD"])
 def index():
     result = {}
     if request.method == "POST":
